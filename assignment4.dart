c4121246a578a199d1e07fb7e511680303227c56
Hamza Shoaib <h4hamzashoaib@gmail.com>
2024-02-07 22:36:15 +0500

    First Question

---
+++ assignment4.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Question no 1
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (final i in numbers) {
+    if (i % 2 == 0) {
+      print('Even numbers $i');
+    }
+  }
+    print('----------------*----------------*-------------');
+}
