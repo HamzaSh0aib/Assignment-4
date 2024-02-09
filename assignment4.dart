daa9c93c6fb8fe438fcc30f62fcd288fb1337e0d
Hamza Shoaib <h4hamzashoaib@gmail.com>
2024-02-09 09:53:16 +0500

    Second Question

--- assignment4.dart
+++ assignment4.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   // Question no 1
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -6,5 +8,22 @@ void main() {
       print('Even numbers $i');
     }
   }
-    print('----------------*----------------*-------------');
+ // Question no 2
+ print('Enter The Number ');
+ int count = int.parse(stdin.readLineSync()!);
+
+ int first =0;
+ int second =1;
+ int next;
+ for (var i = 0; i < count; i++) {
+   if(i<=1){
+    next =i;
+   }
+   else{
+    next = first+second;
+    first =second;
+    second = next;
+   }
+   stdout.write('$next');
+ }
 }
